3bf27a49c1588ba2905eca4c3a4bde7039d14154
ekater1na <ekaterina_l@tut.by>
2022-07-08 22:41:08 +0300

    build: add student res

---
+++ src/graphql.ts
@@ -0,0 +1,38 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+export interface CreateStudentInput {
+    exampleField?: Nullable<number>;
+}
+
+export interface UpdateStudentInput {
+    id: number;
+}
+
+export interface Student {
+    _id?: Nullable<string>;
+    FirstName?: Nullable<string>;
+    LastName?: Nullable<string>;
+    Standard?: Nullable<number>;
+    FatherName?: Nullable<string>;
+    MotherName?: Nullable<string>;
+}
+
+export interface IQuery {
+    getAllStudents(): Nullable<Student>[] | Promise<Nullable<Student>[]>;
+    getStudentById(id?: Nullable<string>): Nullable<Student> | Promise<Nullable<Student>>;
+}
+
+export interface IMutation {
+    createStudent(createStudentInput: CreateStudentInput): Student | Promise<Student>;
+    updateStudent(updateStudentInput: UpdateStudentInput): Student | Promise<Student>;
+    removeStudent(id: number): Nullable<Student> | Promise<Nullable<Student>>;
+}
+
+type Nullable<T> = T | null;
